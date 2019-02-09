8e8abb1e91078160434a4d07c22f7d9a150bb074
Antonio <antoniodaria@msn.com>
2019-02-09 19:42:23 +0000

    renamed @web_pages to @visits

--- lib/counter.rb
+++ lib/counter.rb
@@ -1,24 +1,24 @@
 class Counter
-  attr_reader :web_pages
+  attr_reader :visits
 
   def initialize
-    @web_pages = {}
+    @visits = {}
   end
 
 
   def store_pages(server_log)
     File.open(server_log).each do |log_rows|
       splitted_log_rows = log_rows.split(" ")
-      if @web_pages.has_key?(splitted_log_rows[0])
-       @web_pages[splitted_log_rows[0]] << (splitted_log_rows[1])
+      if @visits.has_key?(splitted_log_rows[0])
+       @visits[splitted_log_rows[0]] << (splitted_log_rows[1])
      else
-       @web_pages[splitted_log_rows[0]] = [splitted_log_rows[1]]
+       @visits[splitted_log_rows[0]] = [splitted_log_rows[1]]
      end
    end
   end
 
   def count_visits
-    sorted_pages = @web_pages.sort_by { |key, value| value.length }.reverse
+    sorted_pages = @visits.sort_by { |key, value| value.length }.reverse
     sorted_pages.each do |views, visits|
        puts "#{views} #{visits.length} visits"
     end
--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -7,7 +7,7 @@ describe Counter do
   describe "#store_pages" do
     it 'stores pages to a hash structure' do
     subject.store_pages("./mockedlog.log")
-      expect(subject.web_pages).to eq({ "/about" => ["061.945.150.735"],
+      expect(subject.visits).to eq({ "/about" => ["061.945.150.735"],
          "/about/2" => ["444.701.448.104"], "/contact" => ["184.123.665.067"], "/help_page/1" => ["126.318.035.038",\
          "929.398.951.889", "722.247.931.582", "646.865.545.408"],\
          "/home" => ["184.123.665.067", "235.313.352.950"],\
